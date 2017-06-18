4ca4bf40ab06719a0438e745ecddebcdb976d699
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2017-06-18 22:09:12 +0200

    Added moar timeout

--- travis-before.sh
+++ travis-before.sh
@@ -3,7 +3,7 @@ echo "Creating Couches on Docker"
 docker run -d --name couchdb2 -p 5985:5984 klaemo/couchdb:1.6.1
 docker run -d --name couchdb -p 5984:5984 klaemo/couchdb:1.6.1 --link=couchdb2:slave
 echo "Waiting for Couches to initialize..."
-for ((i=1;i<=10;i++)); do
+for ((i=1;i<=30;i++)); do
     sleep 1
     printf "."
 done
