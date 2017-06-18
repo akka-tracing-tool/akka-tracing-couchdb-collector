4665a0db36d0908edc42dcce04d26fe4a782a157
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2017-06-18 22:12:23 +0200

    Corrected docker cmd line

--- travis-before.sh
+++ travis-before.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "Creating Couches on Docker"
 docker run -d --name couchdb2 -p 5985:5984 klaemo/couchdb:1.6.1
-docker run -d --name couchdb -p 5984:5984 klaemo/couchdb:1.6.1 --link=couchdb2:slave
+docker run -d --name couchdb --link=couchdb2:slave -p 5984:5984 klaemo/couchdb:1.6.1
 echo "Waiting for Couches to initialize..."
-for ((i=1;i<=30;i++)); do
+for ((i=1;i<=10;i++)); do
     sleep 1
     printf "."
 done
